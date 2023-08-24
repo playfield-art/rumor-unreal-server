9a657544a962ddaa8f4b7b7753558a1559bfc586
jannes lambrecht <janneslambrecht@hotmail.com>
2023-08-24 03:17:18 +0200

    update format

--- main.py
+++ main.py
@@ -12,26 +12,28 @@ from pythonosc import udp_client
 try:
     load_dotenv()
 except Exception as e:
-    raise Exception("Error loading environment variables. Make sure you have a valid .env file.") from e
+    raise Exception(
+        "Error loading environment variables. Make sure you have a valid .env file.") from e
 
 # Setup OSC client
 if os.getenv('OSC_IP') and os.getenv('OSC_PORT'):
-	client = udp_client.SimpleUDPClient(os.getenv('OSC_IP'), int(os.getenv('OSC_PORT')))
+    client = udp_client.SimpleUDPClient(
+        os.getenv('OSC_IP'), int(os.getenv('OSC_PORT')))
 # Fetch environment variables
 bearer_token_graphql = os.getenv('BEARER_TOKEN_GRAPHQL')
 db_url = os.getenv('DB_URL_GRAPHQL')
 try:
-  update_interval = int(os.getenv('DB_UPDATE_INTERVAL'))
+    update_interval = int(os.getenv('DB_UPDATE_INTERVAL'))
 except Exception as e:
-	update_interval = 15
-	print(f"Error: {e}")
+    update_interval = 15
+    print(f"Error: {e}")
 UPDATE_GRAPHQL_DATA = (os.getenv('DB_UPDATE_GRAPHQL_DATA', 'False') == 'True')
 DOWNLOAD_FOR_BUILD = (os.getenv('DOWNLOAD_FOR_BUILD', 'False') == 'True')
-if(DOWNLOAD_FOR_BUILD):
-	output_folder = os.getenv('DOWLOAD_FOLDER_QUOTE_FILES_BUILD')
+if (DOWNLOAD_FOR_BUILD):
+    output_folder = os.getenv('DOWLOAD_FOLDER_QUOTE_FILES_BUILD')
 else:
-	output_folder = os.getenv('DOWLOAD_FOLDER_QUOTE_FILES')
-        
+    output_folder = os.getenv('DOWLOAD_FOLDER_QUOTE_FILES')
+
 
 # Load initial data from 'data.json' with error handling
 try:
@@ -52,19 +54,21 @@ headers = {
     'Authorization': f'Bearer {bearer_token_graphql}'
 }
 
+
 def get_next_update_time() -> datetime:
     today = datetime.now()
     next_update = today + timedelta(minutes=update_interval)
     print(f"Next update: {next_update.strftime('%d/%m/%Y %H:%M:%S')}")
     return next_update
 
-def update_database(force_update = False):
-    if(force_update):
-      print("Forcing update")
+
+def update_database(force_update=False):
+    if (force_update):
+        print("Forcing update")
     else:
-      scheduler.add_job(update_database, 'date', run_date=get_next_update_time())
-    # try:
-    if True:
+        scheduler.add_job(update_database, 'date',
+                          run_date=get_next_update_time())
+    try:
         print("Updating database...")
         brainjar_data = get_brainjar_data()
         interation_id = get_data_from_json('id.json')
@@ -74,47 +78,51 @@ def update_database(force_update = False):
             print("No new data available")
             return
         else:
-          print("New data available")
-          languages = get_languages(headers, db_url)
-          if(UPDATE_GRAPHQL_DATA):
-            print("Update graphql data")
-            graphql_data = get_data(headers, db_url)
-            graphql_data_sanitized = sanitize_data(graphql_data)
-            all_data = format_rumor_data(brainjar_data, graphql_data_sanitized, languages)
-          else:
-            print("Don't update graphql data")
-            all_data = update_rumor_data(brainjar_data, get_data_from_json('data.json'), languages)
-          data_to_use = all_data
-          data_to_use['meta_data'] = {
-            'last_updated': datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
-            'languages': languages,
-            'intro_url': 'intro'
+            print("New data available")
+            languages = get_languages(headers, db_url)
+            if (UPDATE_GRAPHQL_DATA):
+                print("Update graphql data")
+                graphql_data = get_data(headers, db_url)
+                graphql_data_sanitized = sanitize_data(graphql_data)
+                all_data = format_rumor_data(
+                    brainjar_data, graphql_data_sanitized, languages)
+            else:
+                print("Don't update graphql data")
+                all_data = update_rumor_data(
+                    brainjar_data, get_data_from_json('data.json'), languages)
+            data_to_use = all_data
+            data_to_use['meta_data'] = {
+                'last_updated': datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+                'languages': languages,
+                'intro_url': 'intro'
             }
 
-          # Save the updated data to 'data.json'
-          if data_to_use:
-            with open('data.json', 'w') as outfile:
-                json.dump(data_to_use, outfile)
-            # trigger unreal engine
-            if(UPDATE_GRAPHQL_DATA):                                          
-              download_all_audio(graphql_data_sanitized, output_folder)
-            client.send_message("/update", "")
-            print("Update complete")
-            try:
-              with open('id.json', 'w') as outfile:
-                json.dump(brainjar_data['iteration_id'], outfile)
-            except Exception as e:
-              print(f"Error updating id: {e}")          
+            # Save the updated data to 'data.json'
+            if data_to_use:
+                with open('data.json', 'w') as outfile:
+                    json.dump(data_to_use, outfile)
+                # trigger unreal engine
+                if (UPDATE_GRAPHQL_DATA):
+                    download_all_audio(graphql_data_sanitized, output_folder)
+                client.send_message("/update", "")
+                print("Update complete")
+                try:
+                    with open('id.json', 'w') as outfile:
+                        json.dump(brainjar_data['iteration_id'], outfile)
+                except Exception as e:
+                    print(f"Error updating id: {e}")
 
-      	# trigger unreal engine
+        # trigger unreal engine
 
-    # except Exception as e:
-    #     print(f"Error updating database: {e}")
+    except Exception as e:
+        print(f"Error updating database: {e}")
+
+
 # The job will be executed on the next update time
 scheduler.add_job(update_database, 'date', run_date=get_next_update_time())
 
 
-update_database(True)
+# update_database(True)
 
 @app.get("/api/data")
 async def get_data_api() -> dict:
@@ -122,6 +130,7 @@ async def get_data_api() -> dict:
         return get_data_from_json('data.json')
     else:
         raise HTTPException(status_code=500, detail="Data is not available.")
+
 
 @app.get("/api/categories")
 async def get_categories_api() -> dict:
